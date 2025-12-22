58e0f1668f9aadf9a73ed41dfd04ec408646d637
kmos <kmos@commonhaus.dev>
2025-12-22 16:13:43 +0100

    debezium/dbz#1469 introduce DebeziumSerialization
    Signed-off-by: kmos <kmos@commonhaus.dev>

---
+++ quarkus-debezium-parent/quarkus-debezium-engine-spi-parent/runtime/src/main/java/io/debezium/runtime/DebeziumSerialization.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.runtime;
+
+import io.debezium.common.annotation.Incubating;
+import io.debezium.engine.format.SerializationFormat;
+
+/**
+ * serialization information associated to a running {@link Debezium} engine
+ */
+@Incubating
+public interface DebeziumSerialization {
+
+    /**
+     *
+     * @return the key {@link SerializationFormat}
+     */
+    Class<? extends SerializationFormat<?>> getKeyFormat();
+
+    /**
+     *
+     * @return the value {@link SerializationFormat}
+     */
+    Class<? extends SerializationFormat<?>> getValueFormat();
+
+    /**
+     *
+     * @return the header {@link SerializationFormat}
+     */
+    Class<? extends SerializationFormat<?>> getHeaderFormat();
+
+    /**
+     *
+     * @return the id for the {@link Debezium} engine
+     */
+    String getEngineId();
+}
