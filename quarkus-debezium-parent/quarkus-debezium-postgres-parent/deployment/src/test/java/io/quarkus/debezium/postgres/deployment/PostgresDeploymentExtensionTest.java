b132b83f63e625fe19e7b2fc8ae3c27a8d682609
kmos <kmos@commonhaus.dev>
2025-12-30 19:31:05 +0100

    debezium/dbz#1412 move to test suite
    Signed-off-by: kmos <kmos@commonhaus.dev>

---
+++ quarkus-debezium-parent/quarkus-debezium-postgres-parent/deployment/src/test/java/io/quarkus/debezium/postgres/deployment/PostgresDeploymentExtensionTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.quarkus.debezium.postgres.deployment;
+
+import org.junit.platform.suite.api.AfterSuite;
+import org.junit.platform.suite.api.BeforeSuite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+import io.quarkus.debezium.testsuite.deployment.QuarkusDebeziumSqlExtensionTestSuite;
+
+@SuiteDisplayName("Sql Server Debezium Extensions for Quarkus Test Suite")
+public class PostgresDeploymentExtensionTest implements QuarkusDebeziumSqlExtensionTestSuite {
+    private static final PostgresResource postgresResource = new PostgresResource();
+
+    @BeforeSuite
+    public static void init() {
+        postgresResource.start();
+    }
+
+    @AfterSuite
+    public static void close() {
+        postgresResource.stop();
+    }
+}
