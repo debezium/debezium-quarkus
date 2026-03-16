b4940ff5afd0ae966ae692194eddf653df3a5deb
kmos <kmos@commonhaus.dev>
2026-03-16 22:20:19 +0100

    debezium/dbz#1522 add additional values
    Signed-off-by: kmos <kmos@commonhaus.dev>

--- debezium-quarkus-parent/debezium-quarkus-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/PostgresReplicaEnhancer.java
+++ debezium-quarkus-parent/debezium-quarkus-postgres-parent/runtime/src/main/java/io/quarkus/debezium/engine/PostgresReplicaEnhancer.java
@@ -6,6 +6,9 @@
 
 package io.quarkus.debezium.engine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.debezium.connector.postgresql.PostgresConnectorConfig;
 import io.debezium.runtime.Connector;
 
@@ -14,6 +17,14 @@ public class PostgresReplicaEnhancer extends ReplicaConfigurationEnhancer {
     @Override
     public String property() {
         return PostgresConnectorConfig.SLOT_NAME.name();
+    }
+
+    @Override
+    public Map<String, String> additionalValues() {
+        HashMap<String, String> additionalValues = new HashMap<>();
+        additionalValues.put(PostgresConnectorConfig.DROP_SLOT_ON_STOP.name(), "true");
+
+        return additionalValues;
     }
 
     @Override
